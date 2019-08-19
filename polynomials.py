2e864206a8af3d5b90738610456ef876bf59c623
zankovich <skydraft@yandex.ru>
2019-08-19 21:01:31 +0700

    test yandex OK

--- polynomials.py
+++ polynomials.py
@@ -5,25 +5,33 @@
 #output:[1, 2, 3, 5, 8, 12]
 # 1x^5+2x^4+3x^3+5x^2+8x+12
 
-
-a=[6,11]
-b=[1, 2 ,3,5,2,1]
-c=[]
+c1=int(input())
+c=input()
+a= [int(k) for k in c.split()]
+c2=input()
+c=input()
+b= [int(k) for k in c.split()]
+z=[]
 if len(a)>=len(b):
+
     m = len(a) - len(b)
+    print(c1)
     for i in range(m):
         b=[0]+b
 
+
     for i in range(0,len(a),1):
-        c.append(a[i]+b[i])
+        z.append(a[i]+b[i])
 
 else:
     m=len(b)-len(a)
+    print(c2)
     for i in range(m):
         a=[0]+a
 
     for i in range(0,len(b),1):
 
-        c.append(b[i]+a[i])
+        z.append(b[i]+a[i])
 
-print(c)
+print(*z)
+
