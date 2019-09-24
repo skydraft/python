8e9366cba25c275c0d76d083f4abf275baf8f883
zankovich <o.zankovich@cft.ru>
2019-09-24 16:43:31 +0700

    codewars numeretic v1

--- numericals.py
+++ numericals.py
@@ -1,48 +1,15 @@
 def numericals(s):
 
-    sum1=1
+    st = ''
+    count=0
 
-    st='1'
-    s1=s[0]
+    for i in s:
+        count+=1
+        t=s[:count].count(i)
+        st+=str(t)
 
-    print()
-    for i in s[1:]:
-        for j in s1:
-            if j==i:
-                sum1+=1
-        if sum1==1:
-            st +='1'
-        else:
-            st += str(sum1)
-            sum1=1
-        s1+=i
 
     return st
 
-#__________________________________________
-
-def numericals1(s):
-
-    sum1=1
-
-    st='1'
-    s1=s[0]
-    d=dict.fromkeys(s)
-
-    for i in s[1:]:
-        for j in s1:
-            if j==i:
-                sum1+=1
-        if sum1==1:
-            st +='1'
-        else:
-            st += str(sum1)
-            sum1=1
-        s1+=i
-
-    return st
-#print(numericals("à¼ÅÑã9LxÏ±{çµ*±wzN½«Èp¯ÛEØ@!¨ ·L~rË9fOf«&GPS¢­"))
-#11111111111111121111111111111111121111211221111211111
-print(numericals1("hello"))
 
 
