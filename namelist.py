e04ed2628fd1a37bf12b75ee0ed71e6a55664fc0
zankovich <o.zankovich@cft.ru>
2019-09-26 13:26:02 +0700

    codewar: namelist v1.2 add comment

--- namelist.py
+++ namelist.py
@@ -1,3 +1,18 @@
+#Given: an array containing hashes of names
+#Return: a string formatted as a list of names separated by commas except for the last two names,
+# which should be separated by an ampersand.
+#Example:
+#namelist([ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ])
+# returns 'Bart, Lisa & Maggie'
+#namelist([ {'name': 'Bart'}, {'name': 'Lisa'} ])
+# returns 'Bart & Lisa'
+#namelist([ {'name': 'Bart'} ])
+# returns 'Bart'
+#namelist([])
+# returns ''
+
+
+
 def namelist(names):
     st1=''
     if len(names)==0:
